69815ac4f3171fc25be87a36fa7aef8d26410302
rezon <rezonint@gmail.com>
2023-06-15 13:58:27 +0500

    first commit

---
+++ GIT_CONSOLE/Classes/FileWork.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIT_CONSOLE.Classes
+{
+    public static class FileWork
+    {
+        public static string ReadFile(string fileName)
+        {
+            var file = new StreamReader(fileName);
+            string result = file.ReadToEnd();
+            file.Close();
+            return result;
+        }
+        public static void WriteFile (string fileName, string text)
+        {
+            var file = new StreamWriter(fileName);
+            file.WriteLine(text);
+            file.Close();
+        }
+    }
+}
---
+++ GIT_CONSOLE/Program.cs
@@ -0,0 +1,53 @@
+﻿using GIT_CONSOLE.Classes;
+
+Console.WriteLine("Введите команду: ");
+var rezon = Console.ReadLine().Split(' ');
+
+try
+{
+    switch (rezon[0].ToLower())
+    {
+        case "heгp":
+        {
+
+            break;
+        }
+        case "caesar":
+        {
+
+            break;
+        }
+        case "decrypt_caesar":
+        {
+
+            break;
+        }
+        case "sub":
+        {
+
+            break;
+        }
+        case "decrypt_sub":
+        {
+
+            break;
+        }
+        case "vigenere":
+        {
+
+            break;
+        }
+        case "decrypt_vigenere":
+        {
+
+            break;
+        }
+    }
+}
+catch(Exception ex)
+{
+
+}
+
+Console.WriteLine(Vagner.Encrypt("Привет"));
+Console.WriteLine(Vagner.Decrypt("НЧ58УР"));
